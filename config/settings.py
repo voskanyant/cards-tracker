43d453871f4a114c85a05e71fff33fc2168fee80
Tigran Voskanyan <C:\Users\tigra\AppData\Roaming\The Bat!>
2026-01-04 14:31:57 +0300

    Set STATIC_ROOT for collectstatic

--- config/settings.py
+++ config/settings.py
@@ -119,3 +119,5 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
