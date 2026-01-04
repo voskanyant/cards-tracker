e11172c695177117e83d8e7a503a3c4dcad0eb04
Tigran Voskanyan <C:\Users\tigra\AppData\Roaming\The Bat!>
2026-01-04 14:49:42 +0300

    Auto-create superuser on deploy

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
-echo "Current directory:"
-pwd
-echo "Files here:"
-ls -la
-
 pip install -r ./requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+# Create admin user (won’t fail deploy if it already exists)
+python manage.py createsuperuser --noinput || true
