26d7c6d69f5103839126249f0fdd1f90f2097536
Tigran Voskanyan <C:\Users\tigra\AppData\Roaming\The Bat!>
2026-01-04 14:44:41 +0300

    Auto-create admin user on startup

--- core/apps.py
+++ core/apps.py
@@ -1,5 +1,21 @@
 from django.apps import AppConfig
-
+from django.contrib.auth import get_user_model
+import os
 
 class CoreConfig(AppConfig):
-    name = 'core'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "core"
+
+    def ready(self):
+        User = get_user_model()
+
+        username = os.environ.get("ADMIN_USERNAME", "admin")
+        password = os.environ.get("ADMIN_PASSWORD", "Tigran091426510")
+        email = os.environ.get("ADMIN_EMAIL", "tigranvoskanyan1993@gmail.com")
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password,
+            )
