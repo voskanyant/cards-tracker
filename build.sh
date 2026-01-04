81d7060ddfa0c84c639e16bd4ac8957b6608a3ac
Tigran Voskanyan <C:\Users\tigra\AppData\Roaming\The Bat!>
2026-01-04 14:54:57 +0300

    Fix deploy: remove ready() admin creation, create superuser after migrate

--- build.sh
+++ build.sh
@@ -5,5 +5,4 @@ pip install -r ./requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-# Create admin user (won’t fail deploy if it already exists)
 python manage.py createsuperuser --noinput || true
