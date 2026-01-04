55ea4239d2d3042cd073b8efd806203f37b4a855
Tigran Voskanyan <C:\Users\tigra\AppData\Roaming\The Bat!>
2026-01-04 14:19:48 +0300

    Fix requirements.txt path for Render

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
+echo "Current directory:"
+pwd
+echo "Files here:"
+ls -la
+
+pip install -r ./requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
